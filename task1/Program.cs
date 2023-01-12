db244dc08a52b50a52a27ff7fc9d1dd5cfbaabe7
Ilya <osetia9595@gmail.com>
2023-01-12 14:07:49 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+Console.WriteLine("input array size");
+int size = int.Parse(Console.ReadLine());
+int[] array = new int[size];
+FillArray(array);
+int count = 0;
+
+for (int z = 0; z < array.Length; z++)
+if (array[z] % 2 == 0)
+count++;
+
+Console.WriteLine($"{count} even");
+
+void FillArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,999);
+        Console.Write(array[i] + ", ");
+    }
+}
