e6274a915533669c2b082e54ade01dee4328bd51
Ilya <osetia9595@gmail.com>
2023-01-12 14:23:46 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("input array size");
+int size = int.Parse(Console.ReadLine());
+int[] array = new int[size];
+FillArray(array);
+int sum = 0;
+
+
+void FillArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(1, 1000);
+        Console.Write(array[i] + ", ");
+    }
+}
+
+for (int z = 0; z < array.Length; z+=2)
+    sum = sum + array[z];
+
+    Console.WriteLine($"sum uneven numbers = {sum}");
